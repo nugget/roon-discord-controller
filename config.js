84c52f7c90a1a79e46cea5cc63cfb225bf7199f3
David McNett <nugget@macnugget.org>
2020-04-09 16:58:04 +0000

    Restructure settings page for clarity

--- config.js
+++ config.js
@@ -6,14 +6,19 @@ DefaultConfig = {
     bottoken: "",
     channelid: "",
     announcetracks: false,
+    setpresence: true,
     voicechannelid: "",
     streamerid: "",
+    streamingzone: "",
+    localzone: "",
 };
 
 
 function load(roon) {
     return roon.load_config("settings") || DefaultConfig;
 }
+
+// https://community.roonlabs.com/t/settings-api-can-make-a-remote-crash/35899/4?u=nugget
 
 function layout(settings) {
     var l = {
@@ -23,33 +28,59 @@ function layout(settings) {
     };
 
     l.layout.push({
-        type: "string",
-        title: "Discord Bot Token",
-        setting: "bottoken"
+        type: "group",
+        title: "Discord Server",
+        items: [
+            {
+                type: "string",
+                title: "Discord Bot Token",
+                setting: "bottoken"
+            },
+            {
+                type: "string",
+                title: "Text Channel ID",
+                setting: "channelid"
+            },
+            {
+                type: "string",
+                title: "Announce plays",
+                setting: "announcetracks",
+            },
+            {
+                type: "string",
+                title: "Update Presence",
+                setting: "setpresence",
+            }
+        ]
     });
 
-    l.layout.push({
-        type: "string",
-        title: "Text Channel ID",
-        setting: "channelid"
-    });
 
     l.layout.push({
-        type: "string",
-        title: "Announce plays to Discord channel",
-        setting: "announcetracks",
-    });
-
-    l.layout.push({
-        type: "string",
-        title: "Voice Channel ID",
-        setting: "voicechannelid"
-    });
-
-    l.layout.push({
-        type: "string",
-        title: "Streamer User ID",
-        setting: "streamerid"
+        type: "group",
+        title: "Streaming Settings",
+        collapsable: true,
+        items: [
+            {
+                type: "string",
+                title: "Voice Channel ID",
+                setting: "voicechannelid"
+            },
+            {
+                type: "string",
+                title: "Streamer User ID",
+                setting: "streamerid"
+            },
+            {
+                type: "zone",
+                title: "Streaming Zone",
+                setting: "streamingzone"
+            },
+            {
+                type: "zone",
+                title: "Local Zone",
+                setting: "localzone"
+            }
+        ]
     });
 
     return l;
